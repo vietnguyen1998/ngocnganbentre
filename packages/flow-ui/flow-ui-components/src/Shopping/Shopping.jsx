28020df170a91346ee9b3a521ab220411723bd70
Viet Nguyen <vnguyen@kicmail.com>
2022-12-22 17:20:14 +0700

    shopping

--- packages/flow-ui/flow-ui-components/src/Shopping/Shopping.jsx
+++ packages/flow-ui/flow-ui-components/src/Shopping/Shopping.jsx
@@ -1,15 +1,42 @@
 import React from 'react'
-import { Box } from 'theme-ui'
-import CardListSlider from '../CardList/CardList.Slider'
-import Card from '@components/Card'
+import { Box, Button, Input, Grid } from 'theme-ui'
+import Card from '@components/CardShop'
 import buildResponsiveVariant from '@components/utils/buildResponsiveVariant'
 import { nodes } from './data'
-
+import { FaSearch } from 'react-icons/fa'
 const variant = ['horizontal-md', 'vertical']
 const cardListVariant = [
   'lists.cards.fixed.horizontal-md',
   'lists.cards.fixed.vertical'
 ]
+
+const styles = {
+  button: {
+    mx: 2,
+    minWidth: 120
+  },
+  input: {
+    ml: 1,
+    pl: '40px'
+  },
+  searchIcon: {
+    flexShrink: 0,
+    position: `absolute`,
+    top: 14,
+    left: 20
+  },
+  form: ({ focus }) => ({
+    display: 'flex',
+    alignItems: `center`,
+    bg: `omegaLight`,
+    borderRadius: `default`,
+    width: `300px`,
+    maxWidth: `none`,
+    borderStyle: `solid`,
+    borderColor: focus ? `omegaLight` : `headerBg`,
+    borderWidth: `md`,
+  }),
+}
 
 const Shopping = React.forwardRef((props, ref) => {
   const {
@@ -32,7 +59,7 @@ const Shopping = React.forwardRef((props, ref) => {
       // onFocus={() => changeSlide(index)}
       //In sliders with fade effect apply loading to the first card only
       loading={false}
-      columns={[1, 2, 3, 3]}
+      columns={[1, 2, 3, 4]}
       omitCategory={true}
       {...node}
     />
@@ -40,12 +67,28 @@ const Shopping = React.forwardRef((props, ref) => {
   debugger
   return (
     <div>
-      <div className='mb-8 mt-8'>xxx</div>
+      <Grid width={[400]} style={{ marginBottom: 24 }}>
+        <Box bg='muted'>
+          <Button variant='dark' sx={styles.button}>
+            Cây Giống
+          </Button>
+          <Button variant='primary' sx={styles.button}>Hoa Kiểng</Button>
+          <Button variant='dark' sx={styles.button}>Cây Trong Nhà</Button>
+        </Box>
+        <Box style={{position: "relative"}}>
+          <FaSearch style={styles.searchIcon} />
+          <Input
+            sx={styles.input}
+            type='text'
+            placeholder='Nhập tên cây giống'
+            aria-label='Search'
+            autoFocus={true}
+          />
+        </Box>
+      </Grid>
       <Box sx={{ variant: cardListVariant }}>{cards}</Box>
     </div>
   )
 })
-
-//Cards List (Fixed or Slider)
 
 export default Shopping
